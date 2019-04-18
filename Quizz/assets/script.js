ba81f4dda9a43825309827e61a16c069c32cc45a
carinadidomenico <carinadidomenico@gmail.com>
2019-04-18 17:16:47 -0300

    Quiz

---
+++ Quizz/assets/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', function() {
+    
+    let  = document.querySelector ("button");
+    let escolha = document.querySelector ("button").value
+    
+
+    botao.addEventListener('click', function() {
+      });
+
+
+  });
+
+
+//   clicar na opção
+//   armazenar valor
+//   contar um "voto" para o herói
+// o que tiver mais votos aparece na tela
